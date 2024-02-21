d4becb07f22f6309f6e224183bb6d84ce6fe338e
Vladimir Milinic <vladimir.milinic@gmail.com>
2024-02-21 15:40:21 +0100

    created about section background image changing functionality

---
+++ app.js
@@ -0,0 +1,32 @@
+const aboutImgs = [
+   './media/about/about1.jpg',
+   './media/about/about2.jpg',
+   './media/about/about3.jpg',
+   './media/about/about4.jpg',
+   './media/about/about5.jpg',
+   './media/about/about6.jpg',
+];
+const aboutImage = document.getElementById('about-js');
+
+// Preload images
+function preloadImages() {
+   aboutImgs.forEach(function (url) {
+      let img = new Image();
+      img.src = url;
+   });
+}
+
+function startAboutSlideshow() {
+   let index = 1;
+
+   const changeAboutImage = () => {
+      aboutImage.style.backgroundImage = `url('${aboutImgs[index]}')`;
+      index++;
+      if (index === aboutImgs.length) index = 0;
+   };
+
+   setInterval(changeAboutImage, 4000);
+}
+
+preloadImages();
+startAboutSlideshow();
