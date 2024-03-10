a3ce145979d134896c8c3768cc969936ed6518f1
Vladimir Milinic <vladimir.milinic@gmail.com>
2024-03-10 21:38:51 +0100

    completed auto-show reviews

--- app.js
+++ app.js
@@ -7,11 +7,43 @@ const aboutImgs = [
    './media/about/about6.jpg',
 ];
 
+testimonials = [
+   {
+      text: 'Prezadovoljna sam profesionalnošću, izborom i kvalitetom materijala, savetima kako da održavam uradjene nokte i zanoktice koje su mi veliki problem. Kao neko ko nema vremena da sam brine o noktima, ništa mi se ne zakine, niti zalomi, na korekciju dolazim svake 3-4 nedelje. Bukvalno odmorim u prijatnom ambijentu Bellca studija.',
+      name: 'Biljana',
+   },
+   {
+      text: 'Po prvi put mi prirodne trepavice nisu bukvalno otpale nakon svilenih, ne menjam te nikad.',
+      name: 'Ana',
+   },
+   {
+      text: 'Bio me je strah gde da dovedem ćerku koja gricka nokte, a da ne izadjemo iz salona uz alergiju na materijal. Obe smo bile oduševljene. Cene i više nego korektne za ono što nam je pruženo. Uz to izadjete iz salona prezadovoljni i sa osmehom. Mi smo redovni i verni klijenti.',
+      name: 'Milica',
+   },
+   {
+      text: 'Trepavice radim dugo, ne mogu bez njih, kod Branči uvek lepo odremam, i probudim se sa sjajnim trepama koje drže do korekcije.',
+      name: 'Tatjana',
+   },
+   {
+      text: 'Volim prirodne trepavice, samo se opustim, malo i odremam dok se tretman radi i posle mi ni puder na licu ne treba, jer iako radim 1 na 1 tehniku uvek izgledam našminkana. 🥰',
+      name: 'Lana',
+   },
+   {
+      text: 'Kod moje Branči uvek zagarantovan smeh i super razgovor, a nokti i trepavice vrh uvek.',
+      name: 'Slađana',
+   },
+   {
+      text: 'Uvek izadjem iz studia zadovoljna.',
+      name: 'Sanja',
+   },
+];
+
 const aboutImage = document.getElementById('about-js');
 const galleryImgs = document.querySelectorAll('.gallery .image');
 const modal = document.querySelector('.modal-image');
 const modalImg = document.getElementById('modal-js');
 const modalBtn = document.getElementById('modal-close-js');
+const testimonialsContent = document.querySelector('.testimonials .content');
 
 // Preload images
 function preloadImages() {
@@ -54,6 +86,52 @@ const closeModalEsc = (e) => {
    }
 };
 
+let minHeight = 0;
+const reviews = [];
+
+// creating testimonials
+testimonials.forEach((test, index) => {
+   const review = document.createElement('div');
+   review.classList.add('single-review');
+   review.innerHTML = `
+<p class="text-js">
+   ${test.text}
+</p>
+<p class="name">
+   <i class="fa-solid fa-pen-nib"></i>
+   ${test.name}
+</p>`;
+   testimonialsContent.appendChild(review);
+
+   if (review.offsetHeight > minHeight) {
+      minHeight = review.offsetHeight; //calculating min-height depending on reviews
+   }
+
+   if (index === 0) {
+      review.classList.add('show'); //setting first review as active
+   }
+
+   reviews.push(review);
+});
+
+testimonialsContent.style.minHeight = minHeight + 'px'; //setting min-height to reviews container
+
+// auto-change reviews
+const startReviewsChange = () => {
+   let index = 1;
+
+   const changeReview = () => {
+      reviews.forEach((rev) => rev.classList.remove('show'));
+
+      reviews[index].classList.add('show');
+      index++;
+      if (index === reviews.length) index = 0;
+   };
+
+   setInterval(changeReview, 8000);
+};
+
+// EVENT LISTENERS
 galleryImgs.forEach((img) => {
    img.addEventListener('click', () => {
       openModal(img);
@@ -65,3 +143,4 @@ document.addEventListener('keydown', closeModalEsc);
 
 preloadImages();
 startAboutSlideshow();
+startReviewsChange();
